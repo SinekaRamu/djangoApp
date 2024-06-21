cc9a2c2b1702cab6250ab07dfa801500b8b9bd53
SinekaRamu <sineka2001cud@gmail.com>
2024-06-21 17:29:25 +0530

    add-calc

--- myapp/url.py
+++ myapp/url.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path ("", views.home, name="Home"),
-    path ("todos/", views.todos, name="todos")
+    path ("", views.home, name="home"),
+    path ("calc/", views.calc, name="calc"),
+    path ("todo/", views.todos, name="todos"),
+    path ("calc/add", views.add, name="result")
 ] 
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from .models import TodoItem
+from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
+def home(req):
     # return HttpResponse("Hello World!")
-    return render(request, "home.html")
+    return render(req, "home.html", {'name': 'sineka'})
+
+
+def calc(req):
+    return render(req, "result.html")
 
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+def add (request):
+    a = int(request.GET['num1'])
+    b = int(request.GET['num2'])
+    res = a + b
+    return render(request, "result.html", {"result": res})
